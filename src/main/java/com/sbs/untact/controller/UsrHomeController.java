8eb8ffceb97cab2d29a4cc3e84cd234b2ed18760
Anchanghyun <ymy6534@naver.com>
2021-03-24 17:44:30 +0900

    사용자게시물컨트롤러 추가

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,14 +1,34 @@
 package com.sbs.untact.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.untact.dto.Article;
+
+
 @Controller
 public class UsrHomeController {
-	@RequestMapping("/usr/home/main")
+	private List<Article> articles;
+	
+	public UsrHomeController() {
+		articles = new ArrayList<>();
+		
+		articles.add(new Article(1,"2020-12-12 12:12:12","제목1","내용1"));
+		articles.add(new Article(2,"2020-12-13 12:12:12","제목2","내용2"));
+	}
+	@RequestMapping("/usr/home/detail")
 	@ResponseBody
-	public String showMain() {
-		return "안녕1";
+	public Article showdetail(int id) {
+		return articles.get(id - 1);
+	}
+	
+	@RequestMapping("/usr/home/list")
+	@ResponseBody
+	public List<Article> showList() {
+		return articles;
 	}
 }
